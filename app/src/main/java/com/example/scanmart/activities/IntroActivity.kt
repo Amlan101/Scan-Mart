dd719a52d21261e3de68db647df8ca4a22028edb
Amlan <amlan.sahooricky@gmail.com>
2024-08-10 17:09:41 +0530

    Added the Intro Activity and Bottom Nav Bar

---
+++ app/src/main/java/com/example/scanmart/activities/IntroActivity.kt
@@ -0,0 +1,31 @@
+package com.example.scanmart.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.scanmart.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = Intent(this, MainActivity::class.java)
+        binding.startBtn.setOnClickListener {
+            startActivity(intent)
+        }
+
+    }
+}
+
+/*
+1. Home
+2. Map
+-> QR scanner
+3. Cart
+4. Profile
+*/
