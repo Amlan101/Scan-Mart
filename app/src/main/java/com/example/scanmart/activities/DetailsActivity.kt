e796a12dc5f7295090273ddadf53a0ad392fa404
Amlan101 <amlan.sahooricky@gmail.com>
2024-08-11 12:55:19 +0530

    Completed the UI for the details page

---
+++ app/src/main/java/com/example/scanmart/activities/DetailsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.scanmart.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.scanmart.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+    }
+}
